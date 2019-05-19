1e83f25e40e7d8bbd2c5f7620669d14d0670b1ab
erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>
2019-05-19 14:12:04 -0400

    Use functions, better test page

--- www/testPage/test.php
+++ www/testPage/test.php
@@ -3,23 +3,37 @@
 require __DIR__ . "/../../vendor/autoload.php";
 use GuzzleHttp\Client;
 
-// Initialize guzzle Client
-$client = new Client([
-    'timeout'  => 2.0,
-]);
-
-// Ping google and check response
-$displayString = "FAILED using Guzzle to ping google!";
-try {
-    $response = $client->request('GET', 'www.google.com/test');
-    if ($response->getStatusCode() == 200) {
-        $displayString = "SUCCESS using Guzzle to ping google!";
-    }
-}
-catch (Exception $e) {
-    $displayString = "PHP Exception caught using Guzzle to ping google! " . $e->getMessage();
-}
-
+$displayString1 = testURL("www.google.com");
+$displayString2 = testURL("www.google.com/failTest");
 include "test.html";
+
+/**
+ * Makes a GET request to the specified url.
+ *
+ * @param string  $url  The url to make a GET request to
+ * 
+ * @throws none
+ * @return resultString
+ */ 
+function testURL($url) {
+    // Initialize guzzle Client
+    $client = new Client([
+        'timeout'  => 2.0,
+    ]);
+
+    // Ping the url with a GET request and check the response
+    $resultString = "FAILED using Guzzle to ping {$url}!";
+    try {
+        $response = $client->request("GET", $url);
+        if ($response->getStatusCode() == 200) {
+            $resultString = "SUCCESS using Guzzle to ping {$url}!";
+        }
+    }
+    catch (Exception $e) {
+        $resultString = "PHP Exception caught using Guzzle to ping {$url}! " . $e->getMessage();
+    }
+
+    return $resultString;
+}
 
 ?>
