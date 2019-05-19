1b67f510d18d104ad60647b4edb244d07784f5bd
erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>
2019-05-18 21:51:44 -0400

    Upgrade to bento/centos-7.6 and php 7.3

---
+++ box-init/scripts/before/10_php73.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum -y install yum-utils
+yum-config-manager --enable remi-php73
+yum -y install php php-opcache php-xml php-pecl-xdebug
--- box-init/scripts/before/30_composer.sh
+++ box-init/scripts/before/30_composer.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 
-curl -sS https://getcomposer.org/installer | php
+EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+RESULT=$?
 mv composer.phar /usr/local/bin/composer
+rm composer.phar
+rm composer-setup.php
+exit $RESULT
