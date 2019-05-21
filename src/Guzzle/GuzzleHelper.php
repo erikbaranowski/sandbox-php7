5cc187dfb067b4d796407cc3804846cc3a33a64b
erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>
2019-05-20 21:43:36 -0400

    Full unit test coverage except for explicitly ignored function

--- src/Guzzle/GuzzleHelper.php
+++ src/Guzzle/GuzzleHelper.php
@@ -8,6 +8,7 @@ use GuzzleHttp\Client;
 class GuzzleHelper {
 
     protected $guzzleClient;
+    protected $guzzleClientOptions;
 
     /**
      * Constructor - initialize properties.
@@ -19,15 +20,12 @@ class GuzzleHelper {
      */ 
     function __construct($options = null) {
         // Use options if they are passed in, otherwise set using default.
-        $clientOptions = $options;
-        if (is_null($clientOptions)) {
-            $clientOptions = [
+        $this->guzzleClientOptions = $options;
+        if (is_null($this->guzzleClientOptions)) {
+            $this->guzzleClientOptions = [
                 'timeout'  => 2.0,
             ];
         }
-
-        // Initialize guzzle Client
-        $this->guzzleClient = new Client($clientOptions);
     }
 
     /**
@@ -42,12 +40,16 @@ class GuzzleHelper {
         // Ping the url with a GET request and check the response
         $responseDescription = "";
         try {
-            $response = $this->guzzleClient->request("GET", $url);
+            $client = $this->getGuzzleClient();
+            $response = $client->request("GET", $url);
             if ($response->getStatusCode() == 200) {
                 $responseDescription = "SUCCESS using Guzzle to call {$url}!";
+            } else {
+                $responseDescription = "FAILED using Guzzle to call {$url}!";
             }
         }
         catch (\Exception $e) {
+            $response = null;
             $responseDescription = "PHP Exception caught using Guzzle to call {$url}! " . $e->getMessage();
         }
 
@@ -55,6 +57,34 @@ class GuzzleHelper {
             "response" => $response,
             "responseDescription" => $responseDescription,
         ];
+    }
+
+    /**
+     * Lazy load an instance of a GuzzleHttp\Client.
+     *
+     * @param array  $clientOptions  The constructor params
+     * 
+     * @throws none
+     * @return Client GuzzleHttp\Client
+     */ 
+    protected function getGuzzleClient() {
+        if (!is_a($this->guzzleClient, Client::class)) {
+            $this->guzzleClient = $this->createGuzzleClient();
+        }
+        return $this->guzzleClient;
+    }
+
+    /**
+     * Instantiate an instance of a GuzzleHttp\Client. Broken out for unit testing.
+     *
+     * @codeCoverageIgnore
+     * @param array  $clientOptions  The constructor params
+     * 
+     * @throws none
+     * @return Client GuzzleHttp\Client
+     */ 
+    protected function createGuzzleClient() {
+        return new Client($this->guzzleClientOptions);
     }
 }
 
--- tests/src/Guzzle/GuzzleHelperTest.php
+++ tests/src/Guzzle/GuzzleHelperTest.php
@@ -3,12 +3,71 @@
 namespace tests\src\Guzzle;
 
 use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+use src\Guzzle\GuzzleHelper;
 
 class GuzzleHelperTest extends TestCase
 {
-    public function testTrueIsTrue()
+    protected const TEST_URL = "www.something.com";
+    protected const SUCCESS = 200;
+    protected const SERVER_ERROR = 500;
+
+    protected $mockClient;
+    protected $mockResponse;
+    protected $mockGuzzleHelper;
+
+
+    protected function setUp(): void {
+        $this->mockResponse = $this->getMockBuilder("GuzzleHttp\Psr7\Response")
+            ->disableOriginalConstructor()
+            ->setMethods(['getStatusCode'])
+            ->getMock();
+
+        $this->mockClient = $this->getMockBuilder("GuzzleHttp\Client")
+            ->disableOriginalConstructor()
+            ->setMethods(['request'])
+            ->getMock();
+        $this->mockClient->method('request')->willReturn($this->mockResponse);
+
+        $this->mockGuzzleHelper = $this->getMockBuilder("src\Guzzle\GuzzleHelper")
+            ->setConstructorArgs([])
+            ->setMethods(['createGuzzleClient'])
+            ->getMock();
+        $this->mockGuzzleHelper->method('createGuzzleClient')->willReturn($this->mockClient);
+    }
+
+    protected function tearDown(): void {
+        
+    }
+
+    public function testMakeGetRequestSuccess()
     {
-        $foo = true;
-        $this->assertTrue($foo);
+        $this->mockClient->method('request')->willReturn($this->mockResponse);
+        $this->mockResponse->method('getStatusCode')->willReturn(self::SUCCESS);
+        $result = $this->mockGuzzleHelper->makeGetRequest(self::TEST_URL);
+        $expectedString = "SUCCESS using Guzzle to call " . self::TEST_URL . "!";
+        $this->assertSame($expectedString, $result["responseDescription"]);
+        $this->assertSame(self::SUCCESS, $result["response"]->getStatusCode());
+    }
+
+    public function testMakeGetRequestFail()
+    {
+        $this->mockClient->method('request')->willReturn($this->mockResponse);
+        $this->mockResponse->method('getStatusCode')->willReturn(self::SERVER_ERROR);
+        $result = $this->mockGuzzleHelper->makeGetRequest(self::TEST_URL);
+        $expectedString = "FAILED using Guzzle to call " . self::TEST_URL . "!";
+        $this->assertSame($expectedString, $result["responseDescription"]);
+        $this->assertSame(self::SERVER_ERROR, $result["response"]->getStatusCode());
+    }
+
+    public function testMakeGetRequestException()
+    {
+        $exceptionText = "Exception!";
+        $this->mockClient->method('request')->will($this->throwException(new \Exception($exceptionText)));
+        $result = $this->mockGuzzleHelper->makeGetRequest(self::TEST_URL);
+        $expectedString = "PHP Exception caught using Guzzle to call " . self::TEST_URL . "! " . $exceptionText;
+        $this->assertSame($expectedString, $result["responseDescription"]);
+        $this->assertNull($result["response"] ?? null);
     }
 }
