cb1b24ffe3657c104a235eee966068332a3d1dae
erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>
2019-05-24 20:59:38 -0400

    install php-zip

--- box-init/scripts/before/10_php73.sh
+++ box-init/scripts/before/10_php73.sh
@@ -4,4 +4,4 @@ yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarc
 yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 yum -y install yum-utils
 yum-config-manager --enable remi-php73
-yum -y install php php-opcache php-xml php-pecl-xdebug php-mbstring
+yum -y install php php-opcache php-xml php-pecl-xdebug php-mbstring php-zip
